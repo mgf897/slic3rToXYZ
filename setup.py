a38030bec16ad6b3267c901da24be2391b8465be
Matthew Flynn <mattf@ringway.com.au>
2016-01-25 10:10:50 +1100

    Updated script to remove unwanted gcode. Exe is now 32 bit

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ sys.argv.append('py2exe')
 
 setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': "slic3rToXYZ.py"}],
+    windows = [{'script': "C:\\Users\mattf\Desktop\Personal\slic3rToXYZ\slic3rToXYZ.py"}],
     zipfile = None,
 )
--- slic3rToXYZ.py
+++ slic3rToXYZ.py
@@ -6,32 +6,26 @@ parser.add_argument("gcode")
 args = parser.parse_args()
 
 new_filename = args.gcode.split(".")[0] + ".3w"
-new_unencoded_filename = args.gcode.split(".")[0] + "_unencoded.3w"
 
-header_XYZ = "; filename = composition.3w\r\n; machine = daVinciF10\r\n; material = default\r\n; layer_height = 0.2\r\n; fill_density = 0.10\r\n; shells = 3\r\n; speed = 60\r\n; total_layers = 124\r\n; total_filament = 2116.02\r\n; dimension = 51.23:45.21:24.95\r\n; extruder = 1"
+header_XYZ = "; filename = composition.3w\r\n; machine = daVinciF10\r\n; material = default\r\n; layer_height = 0.2\r\n; fill_density = 0.10\r\n; shells = 3\r\n; speed = 60\r\n; total_layers = 124\r\n; total_filament = 2116.02\r\n; dimension = 51.23:45.21:24.95\r\n; extruder = 1\r\n"
 
 encoded_string = ""
 gcode_stripped = ""
 line = ""
 n = 0
+keep_lines = False
 
 with open(args.gcode, "r") as gcode_file:
 	for i in gcode_file:
 		n += 1
 		line = str(i)
 
-		if line.startswith(";") and (n < 20):	# remove comments from first 20 lines of file
-			pass
-		else:
+		if (line[0:3] == "G21") or (keep_lines):
 			gcode_stripped += line
+			keep_lines = True
+
 
 encoded_string = base64.b64encode(bytes(header_XYZ + gcode_stripped,'utf-8'))
-
-# Create debug output
-# Comment these line out if desired
-out_file = open(new_unencoded_filename,"w")
-out_file.write(header_XYZ + gcode_stripped)
-out_file.close()
 
 # Create encoded output
 out_file = open(new_filename,"wb")
